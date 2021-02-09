f73ef5dcb9b76aa52b4b65d71204b4413b12461c
Елена Макеева <len16730637@yandex.ru>
2021-02-09 22:26:39 +0300

    Создан React-элемент

--- src/App.js
+++ src/App.js
@@ -1,70 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './numbers';
+import React from 'react';
 
-const number = 46;
-const checkTrue = true;
-const checkFalse = false;
+const element = (<div>
+  Hello word!
+  <a href="">go to</a>
+  </div>);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p style = {{
-            color: 'red'
-          }}>
-            Внесены изменения
-          </p>
-
-          <p>
-            {checkTrue}
-          </p>
-
-          <p>
-            {12+12}
-          </p>
-
-          <p>
-            {checkTrue && 'flag is true'}
-          </p>
-
-          <p>
-            {checkFalse && 'flag is true'}
-          </p>
-
-          <p>
-            {checkTrue ? 'Тернарное условие истинно' : 'Тернарное условие истинно'}
-          </p>
-
-          <p>
-            {true}
-            {false}
-            {null}
-            {undefined}
-          </p>
-
-          <p>
-            count * length ={count*length}
-          </p>
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+export { element };
--- src/numbers.js
+++
@@ -1,4 +0,0 @@
-const count = 3;
-const length = 10;
-
-export { count, length };
