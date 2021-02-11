8a039d8e3012833f6bfb967093d005ce2edccefe
Елена Макеева <len16730637@yandex.ru>
2021-02-11 15:13:00 +0300

    Create folder with components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (<footer>
+	<div>Здесь будет футер</div>
+</footer>);
+
+export default Footer;
---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputItem = () => (<form>
+	<input type="text" value="new task" /> 
+</form>);
+
+export default InputItem;
