a4ba39cde52727cde8c77420cb4cb4c7c2fbecd4
Елена Макеева <len16730637@yandex.ru>
2021-02-12 22:39:07 +0300

    add bib classnames

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import styles from './App.modules.css';
+import styles from './App.module.css';
 
 const todoItem = 'Redux';
 
@@ -20,12 +20,12 @@ const App = () => {
   	];
 
 	return ( 
-	<div className='wrap'>
-		<h1 className='title'>todos</h1>
-		<InputItem />
-		<ItemList items = {items} />
-		<Footer count = {2} />
-	</div>);
+		<div className={styles.wrap}>
+			<h1 className={styles.title}>todos</h1>
+			<InputItem />
+			<ItemList items = {items} />
+			<Footer count = {2} />
+		</div>);
 }
 
 export default App;
