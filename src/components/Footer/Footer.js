a3b4c375f82181c8c07de05f43e24e2c4c56c18f
Елена Макеева <len16730637@yandex.ru>
2021-02-15 22:40:38 +0300

    stylization of the app

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
-const Footer = ({ count }) => (<footer>
-	<div>Вот столько дел осталось сделать: {count}</div>
+const Footer = ({ count }) => (<footer className={styles.footer}>
+	<div>Вот столько дел осталось сделать: <span className={styles.count}>{count}</span></div>
 </footer>);
 
 export default Footer;
