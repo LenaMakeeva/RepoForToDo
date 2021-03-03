58220e3343244b3726df93b8df908cd67f853925
Елена Макеева <len16730637@yandex.ru>
2021-03-03 21:09:52 +0300

    React Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,103 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import InputItem from '../InputItem/InputItem';
-import ItemList from '../ItemList/ItemList';
-import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Todo from '../Todo/Todo';
+import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
+import MenuList from '@material-ui/core/MenuList';
 
-const todoItem = 'Redux';
-
-
-
-const App = () => {
-	const initialState = {
-		items : [
-			{
-				value: 'Создать приложение',
-				isDone: true,
-				id: 1
-			},
-			{
-				value: 'Сделать структуру приложения',
-				isDone: true,
-				id: 2
-			},
-			{
-				value: 'Добавить стили',
-				isDone: false,
-				id: 3
-			}
-		],
-		count : 3
-	};
-
-	const [items,setItems] = useState(initialState.items);
-	const [count,setCount] = useState(initialState.count);
-
-	useEffect(() => {
-		console.log('componentDidMount');
-	}, []);
-
-	useEffect (() => {
-		console.log('componentDidUpdate');
-	});
-
-	// constructor(props) {
-	// 	super(props);
-
-	// 	onClickDone=this.onClickDone.bind(this);
-	// }
-
-
-
-	const onClickDone = id => {
-		const newItemList = items.map(item => { //проверка изменившихся элементов
-			const newItem = { ...item }; //деструктуризация ItemList
-
-			if (item.id === id) { //проверка был нажат элемент или нет
-				newItem.isDone = !item.isDone; //изменение isDone на противоположный 
-			}
-
-			return newItem;
-		});
-
-		setItems(newItemList);
-	};
-
-	const onClickDelete = id => {
-		const newItemList = items.filter(item => item.id !== id);
-
-		setItems(newItemList);
-		setCount((count) => count -1);
-	};
-
-	const onClickAdd = value => { //создаем новый state
-		const newItems = [ // создаем новый массив items
-			...items, //переложение всех существующих item с помощью деструктуризачии, чтобы получился новый
-			{
-				value,
-				isDone: false,
-				id: count + 1
-			}
-		];
-		setItems(newItems);
-		setCount((count) => count + 1);
-	};
-
-	return ( 
+const App = () => 
+	(<Router> 
 		<div className={styles.wrap}>
-			<Card variant="outlined">
+			<Card className={styles.sidebar}>
+				<MenuList>
+					<Link to='/' className={styles.link}><MenuItem>Обо мне</MenuItem></Link>
+					<Link to='/Todo' className={styles.link}><MenuItem>Дела</MenuItem></Link>
+					<Link to='/Contacts' className={styles.link}><MenuItem>Контакты</MenuItem></Link>
+	      		</MenuList>
+	      	</Card>
+			<Card variant="outlined" className={styles.content}>
 	    		<CardContent>
-					<h1 className={styles.title}>Список задач</h1>
-					<InputItem onClickAdd={onClickAdd}/>
-					<ItemList items={items} onClickDone={onClickDone} onClickDelete={onClickDelete}/>
-					<Footer count = {count} />
+	    			<Route path='/' exact component={About} />
+	    			<Route path='/Todo' exact component={Todo} />
+	    			<Route path='/Contacts' exact component={Contacts} />
 				</CardContent>
 			</Card>
 		</div>
-	);
-}
+	</Router>);
 
 export default App;
