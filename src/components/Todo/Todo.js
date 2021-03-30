4a8273fdd228f84984659421c903a33a1b6164a7
Елена Макеева <len16730637@yandex.ru>
2021-03-30 21:42:07 +0300

    update

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -83,7 +83,7 @@ const Todo = () => {
 	return (
 		<Card variant="outlined" className={styles.cardContent}>
 				<h1 className={styles.title}>Список задач</h1>
-				<InputItem items={items} onClickAdd={onClickAdd}/>
+				<InputItem  onClickAdd={onClickAdd}/>
 				<ItemList items={newItemList} onClickDone={onClickDone}	onClickDelete={onClickDelete}/>
 				<Footer filtered={filter} onClickFilter={onClickFilter}	count = {count}	countDone = {countDone}	countUndone = {countUndone}/>
 		</Card>
